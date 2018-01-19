5c73a3984cb697b4ea34718289843592af724383
MZI <macchenjl@foxmail.com>
2018-01-19 21:47:39 +0800

    add test scripts

---
+++ test/result.test.js
@@ -0,0 +1,87 @@
+const server = require('./server'),
+  bodychecker = require('../index'),
+  supertest = require('supertest'),
+  should = require('should'),
+  assert = require('assert')
+
+const client = supertest(server)
+
+describe('Test Bodychecker Results', () => {
+  it('Valid POST data on /user/login', (done) => {
+    client.post('/user/login').send({
+      username: 'mzi',
+      password: 'happyhappycode'
+    })
+      .expect(200, 'OK!', done)
+  })
+
+  it('Invalid POST data on /user/login', (done) => {
+    client.post('/user/login').send({
+      username: 'mzi',
+      password: 123456
+    })
+      .expect('Content-Type', /json/)
+      .expect(401, {
+        message: 'Field path: `password`, value: 123456 is invalid, type "string" expected!',
+        type: 'invalid',
+        fieldpath: 'password',
+        fieldvalue: 123456
+      }, done)
+  })
+
+  it('Valid POST data on /user/edit', (done) => {
+    client.post('/user/edit').send({
+      username: 'mzi',
+      password: 'happyhappycode',
+      friend_ids: ['1000', '1001', '1002'],
+      profile: {
+        nickname: 'mzi',
+        gender: 'male',
+        age: 24
+      }
+    })
+      .expect(200, 'OK!', done)
+  })
+
+  it('Invalid POST data in arrayof on /user/edit', (done) => {
+    client.post('/user/edit').send({
+      username: 'mzi',
+      password: 'happyhappycode',
+      friend_ids: [1000, '1001', '1002'],
+      profile: {
+        nickname: 'mzi',
+        gender: 'male',
+        age: 24
+      }
+    })
+      .expect(401, {
+        "message": "Field path: `friend_ids[0]`, value: 1000 is invalid, type \"string\" expected!",
+        "type": "invalid",
+        "fieldpath": "friend_ids[0]",
+        "fieldvalue": 1000
+      }, done)
+  })
+
+  it('Invalid POST data in shapeof on /user/edit', (done) => {
+    client.post('/user/edit').send({
+      username: 'mzi',
+      password: 'happyhappycode',
+      friend_ids: ['1000', '1001', '1002'],
+      profile: {
+        nickname: 'mzi',
+        gender: 'man',
+        age: 24
+      }
+    })
+      .expect(401, {
+        "message": "Field path: `profile.gender`, value: \"man\" is invalid, can't find the value in the \"oneof\" array!",
+        "type": "invalid",
+        "fieldpath": "profile.gender",
+        "fieldvalue": "man"
+      }, done)
+  })
+
+  afterEach(() => {
+    server.close()
+  })
+})
---
+++ test/server.js
@@ -0,0 +1,44 @@
+const http = require('http'),
+  express = require('express'),
+  bodyparser = require('body-parser'),
+  bodychecker = require('../index')
+
+const app = express()
+
+app.use(bodyparser.json())
+
+const responseHandler = (req, res, next) => {
+  console.info(`${req.method} - ${req.path}`)
+  if (req.$bcResult) {
+    console.log(`req.$bcResult = ${JSON.stringify(req.$bcResult)}`)
+    return res.status(401).json(req.$bcResult)
+  }
+  res.status(200).send('OK!')
+}
+
+app.post('/user/login', bodychecker({
+  username: bodychecker.string.isRequired,
+  password: bodychecker.string.isRequired
+}), responseHandler)
+
+app.post('/user/edit', bodychecker({
+  username: bodychecker.string.isRequired,
+  password: bodychecker.string.isRequired,
+  friend_ids: bodychecker.arrayof(bodychecker.string.isRequired),
+  profile: bodychecker.shapeof({
+    nickname: bodychecker.string,
+    gender: bodychecker.oneof([
+      'male',
+      'femamle'
+    ]),
+    age: bodychecker.number
+  })
+}), responseHandler)
+
+const server = http.createServer(app)
+
+module.exports = server
+
+server.listen(3000, () => {
+  console.log('Test Server Listening on port 3000.')
+})
